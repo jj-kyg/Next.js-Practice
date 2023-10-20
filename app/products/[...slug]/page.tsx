d5f8d43a7b4d6021b410da7d596040162e667ca1
Justin Johnson <jjohnson1@NLN-Admin-1s-MacBook-Air.local>
2023-10-20 15:59:35 -0500

    catch-all segments

---
+++ app/products/[...slug]/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+interface Props {
+  params: { slug: string[] };
+}
+
+const ProductPage = () => {
+  return <div>ProductPage</div>;
+};
+
+export default ProductPage;
